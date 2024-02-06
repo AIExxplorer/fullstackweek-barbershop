6d6a57287a56cae72d05db3d2ad69ca2e0ad885d
Wagner Rafael <contactaiexxplorer@gmail.com>
2024-02-06 19:14:50 -0300

    feat: add initial search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -8,12 +9,16 @@ export default function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold"> Olá, Miguel!</h2>
+        <h2 className="text-xl font-bold"> Olá, Wagner!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
           locale: ptBR,
         })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+      <Search />
       </div>
     </div>
   );
